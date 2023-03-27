efccfa4cade28d15aeb5e58e5b65ec155c020976
Sumit Kumar <messikumar66@gmail.com>
2023-03-27 23:50:00 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -12,15 +12,13 @@ const int N = 1e6 + 5, MOD = 1e9 + 7;
 #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 
 void solve(){
-	/*
-  ordered_set <set_name>
+/* ordered_set <set_name>
   functionalities:
   <set_name>.insert(<element>);
   <set_name>.find_by_order(k); == It returns to an iterator 
   to the kth element (counting from zero) in the set in O(logn) time
   <set_name>.order_of_key(k) == It returns to the number of items 
-  that are strictly smaller than our item k in O(logn) time.
-  */
+  that are strictly smaller than our item k in O(logn) time. */
 }
 
 int32_t main(){
