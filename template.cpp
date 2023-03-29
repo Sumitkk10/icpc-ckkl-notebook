722713e0d14d65f7e783e2d2bc89b3ef8a95e91d
Sumit Kumar <messikumar66@gmail.com>
2023-03-29 11:22:28 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -17,6 +17,16 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int genNum(int l, int r) {
     return l + rand() % (r - l + 1);
 }
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31); }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM); }
+};
 void solve(){
 /* ordered_set <set_name>
   functionalities:
@@ -25,6 +35,7 @@ void solve(){
   to the kth element (counting from zero) in the set in O(logn) time
   <set_name>.order_of_key(k) == It returns to the number of items 
   that are strictly smaller than our item k in O(logn) time. */
+  // unordered_map<long long, int, custom_hash> safe_map;
 }
 int32_t main(){
 	srand(chrono::steady_clock::now().time_since_epoch().count());
