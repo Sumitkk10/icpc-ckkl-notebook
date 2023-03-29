98f9206170ee60291ac322fea731155777cfc6ee
Harshvardhan Pandey <108459560+harshvardhan-pandey@users.noreply.github.com>
2023-03-29 12:17:17 +0530

    Update 2D Fenwick Tree.cpp

--- Data Structures/2D Fenwick Tree.cpp
+++ Data Structures/2D Fenwick Tree.cpp
@@ -1,14 +1,12 @@
 const int mxn = 1000;
 int grid[mxn + 1][mxn + 1];
 int bit[mxn + 1][mxn + 1];
-
 void update(int row, int col, int d){
     grid[row][col] += d;
     for(int i = row; i <= mxn; i += (i & -i))
         for(int j = col; j <= mxn; j += (j & -j))
             bit[i][j] += d;
 }
- 
 int sum(int row, int col){
     //calculates sum from [1,1] till [row,col]
     int res = 0;
